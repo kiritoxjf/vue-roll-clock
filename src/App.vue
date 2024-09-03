02125bb181dff131d54ddbb95e6b94f12e8158c9
kirito <kiritoxjf@outlook.com>
2024-09-03 16:21:57 +0800

    v0.0.1

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import Digit from './components/Digit.vue'
+const props = defineProps({
+	value: {
+		type: String,
+		default: '12:34:56'
+	},
+	background: {
+		type: String,
+		default: '#007175'
+	}
+})
+</script>
+<template>
+	<div class="vue-roll-clock" :style="`background-color: ${props.background}`">
+		<Digit :type="2" :digit="Number(props.value[0])" :background="props.background" />
+		<Digit :digit="Number(props.value[1])" :background="props.background" />
+		<span>:</span>
+		<Digit
+			:type="1"
+			:digit="Number(props.value[3])"
+			:background="props.background" />
+		<Digit :digit="Number(props.value[4])" :background="props.background" />
+		<span>:</span>
+		<Digit
+			:type="1"
+			:digit="Number(props.value[6])"
+			:background="props.background" />
+		<Digit :digit="Number(props.value[7])" :background="props.background" />
+	</div>
+</template>
+<style scoped>
+.vue-roll-clock {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+	width: 100%;
+	font-weight: bold;
+	color: inherit;
+}
+</style>
---
+++ src/components/Digit.vue
@@ -0,0 +1,109 @@
+<script setup>
+const props = defineProps({
+	type: {
+		// 0： 0-9
+		// 1： 0-5
+		// 2： 0-2
+		type: Number,
+		default: 0
+	},
+	digit: {
+		type: Number,
+		default: 0
+	},
+	background: {
+		type: String,
+		default: '#007175'
+	}
+})
+
+const f = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const s = [0, 1, 2, 3, 4, 5]
+const t = [0, 1, 2]
+</script>
+<template>
+	<div class="digit" :style="`background-color: ${props.background}`">
+		<div
+			class="container"
+			v-if="props.type === 0"
+			:style="`transform: rotateX(${-36 * props.digit}deg)`">
+			<div
+				class="board"
+				:style="`transform: rotateX(${36 * props.digit}deg)`"></div>
+			<div
+				class="digit-item"
+				v-for="num in f"
+				:key="num"
+				:style="`--i: ${num}; --j: 36; background-color: ${props.background}`">
+				{{ num }}
+			</div>
+		</div>
+		<div
+			class="container"
+			v-if="props.type === 1"
+			:style="`transform: rotateX(${-60 * props.digit}deg)`">
+			<div
+				class="board"
+				:style="`transform: rotateX(${60 * props.digit}deg)`"></div>
+			<div
+				class="digit-item"
+				v-for="num in s"
+				:key="num"
+				:style="`--i: ${num}; --j: 60; background-color: ${props.background}`">
+				{{ num }}
+			</div>
+		</div>
+		<div
+			class="container"
+			v-if="props.type === 2"
+			:style="`transform: rotateX(${-120 * props.digit}deg)`">
+			<div
+				class="board"
+				:style="`transform: rotateX(${120 * props.digit}deg)`"></div>
+			<div
+				class="digit-item"
+				v-for="num in t"
+				:key="num"
+				:style="`--i: ${num}; --j: 120; background-color: ${props.background}`">
+				{{ num }}
+			</div>
+		</div>
+	</div>
+</template>
+<style scoped>
+.digit {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 2rem;
+	height: 2.4rem;
+	font-size: inherit;
+	perspective: 16rem;
+	overflow: hidden;
+}
+
+.digit .container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	transform-style: preserve-3d;
+	transition: transform 0.5s linear;
+}
+
+.digit .container .board {
+	position: absolute;
+	width: 100%;
+	height: 120%;
+	background: #007175;
+}
+.digit .container .digit-item {
+	position: absolute;
+	display: flex;
+	height: 2rem;
+	justify-content: center;
+	align-items: center;
+	transform: rotateX(calc(var(--i) * var(--j) * 1deg)) translateZ(10rem);
+}
+</style>
